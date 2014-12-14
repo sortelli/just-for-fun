aefd50bc7630f0c3c879f79b0af829cd49dc7ae7
Joe Sortelli <joe@sortelli.com>
2014-12-13 23:32:09 -0800

    Solve puzzle

--- puzzles/11th-hour/books/books.rb
+++ puzzles/11th-hour/books/books.rb
@@ -10,23 +10,62 @@ require 'set'
 #
 # Inital layout:
 #
-#  +----+----+----+----+----+----+----+----+----+----+
-#  | R1 | G1 | R2 | G2 | R3 | G3 | R4 | G4 |    |    |
-#  +----+----+----+----+----+----+----+----+----+----+
-#    0    1    2    3    4    5    6    7    8    9
+#  +---+---+---+---+---+---+---+---+---+---+
+#  | R | G | R | G | R | G | R | G |   |   |
+#  +---+---+---+---+---+---+---+---+---+---+
+#    0   1   2   3   4   5   6   7   8   9
 
 class BooksState < BfsBruteForce::State
-  def initialize
+  def initialize(books = nil)
+    @books = books || [
+      :R, :G,
+      :R, :G,
+      :R, :G,
+      :R, :G,
+      :_,  :_
+    ]
   end
 
   def solved?
-    true
+    @books == [
+      :R, :R,
+      :R, :R,
+      :_, :_,
+      :G, :G,
+      :G, :G
+    ]
   end
 
   def next_states(already_seen)
+    empty_start = @books.index :_
+
+    (@books.length - 1).times.each do |index|
+      next if index == empty_start - 1 or index == empty_start
+
+      book1, book2 = @books[index, 2]
+
+      new_books = @books.dup
+
+      new_books[empty_start]     = book1
+      new_books[empty_start + 1] = book2
+
+      new_books[index]     = :_
+      new_books[index + 1] = :_
+
+      new_state = BooksState.new new_books
+      yield "Move #{index}.5 to #{empty_start}.5\n#{self}\n#{new_state}", new_state
+    end
   end
 
   def to_s
+    fmt = %q{
+      +---+---+---+---+---+---+---+---+---+---+
+      | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s |
+      +---+---+---+---+---+---+---+---+---+---+
+        0   1   2   3   4   5   6   7   8   9
+    }
+
+    fmt % @books.map {|s| s == :_ ? ' ' : s}
   end
 end
 
