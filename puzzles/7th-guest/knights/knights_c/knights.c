584d01bab6c508d25de5fceb613030209c0e31bd
Joe Sortelli <joe@sortelli.com>
2014-12-17 19:50:24 -0800

    Started c version

---
+++ puzzles/7th-guest/knights/knights_c/knights.c
@@ -0,0 +1,27 @@
+/*
+ * Objective: Swap black and white knights
+ *
+ * Inital Board layout:
+ *
+ *    +----+----+----+----+----+
+ *  5 | BL | BL | BL | BL | WH |
+ *    +----+----+----+----+----+
+ *  4 | BL | BL | BL | WH | WH |
+ *    +----+----+----+----+----+
+ *  3 | BL | BL |    | WH | WH |
+ *    +----+----+----+----+----+
+ *  2 | BL | BL | WH | WH | WH |
+ *    +----+----+----+----+----+
+ *  1 | BL | WH | WH | WH | WH |
+ *    +----+----+----+----+----+
+ *      a    b    c    d    e
+ *
+ *   BL = Black Knight
+ *   WH = WHite Knight
+*/
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  return 0;
+}
