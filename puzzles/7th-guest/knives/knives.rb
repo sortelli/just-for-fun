3bb0d9e03dcf657a7ddaf5d5a565232c7ddb9ce4
Joe Sortelli <joe@sortelli.com>
2014-12-22 07:19:30 -0800

    Add to_s and @@moves

--- puzzles/7th-guest/knives/knives.rb
+++ puzzles/7th-guest/knives/knives.rb
@@ -11,11 +11,11 @@ require 'bfs_brute_force'
 #
 #         0
 #        / \
-#   1 - 3 - 4 - 2
+#   1 - 2 - 3 - 4
 #    \ /     \ /
 #     5       6
-#    /  \ 7 /  \
-#    8 /     \ 9
+#     / \ 7 / \
+#     8 /   \ 9
 #
 #
 # A knife can only move if it is immediately next to another knife,
@@ -25,6 +25,29 @@ require 'bfs_brute_force'
 # This is the "Knife" puzzle from an old video game, The 7th Guest.
 
 class KnivesState < BfsBruteForce::State
+  @@moves = [
+    {:from => 0, :to => 5, :jump => 2},
+    {:from => 0, :to => 6, :jump => 3},
+    {:from => 1, :to => 3, :jump => 2},
+    {:from => 1, :to => 7, :jump => 5},
+    {:from => 2, :to => 4, :jump => 3},
+    {:from => 2, :to => 8, :jump => 5},
+    {:from => 3, :to => 1, :jump => 2},
+    {:from => 3, :to => 9, :jump => 6},
+    {:from => 4, :to => 2, :jump => 3},
+    {:from => 4, :to => 7, :jump => 6},
+    {:from => 5, :to => 0, :jump => 2},
+    {:from => 5, :to => 9, :jump => 7},
+    {:from => 6, :to => 0, :jump => 3},
+    {:from => 6, :to => 8, :jump => 7},
+    {:from => 7, :to => 1, :jump => 5},
+    {:from => 7, :to => 4, :jump => 6},
+    {:from => 8, :to => 2, :jump => 5},
+    {:from => 8, :to => 6, :jump => 7},
+    {:from => 9, :to => 5, :jump => 7},
+    {:from => 9, :to => 3, :jump => 6}
+  ]
+
   def initialize(knives)
     @knives = knives
   end
@@ -35,4 +58,23 @@ class KnivesState < BfsBruteForce::State
 
   def next_states(already_seen)
   end
+
+  def to_s
+    fmt = %q{
+            %s
+           / \
+      %s - %s - %s - %s
+       \ /     \ /
+        %s       %s
+        / \ %s / \
+        %s /   \ %s
+    }
+
+    fmt % @knives.map {|k| k ? 'X' : 'O'}
+  end
 end
+
+solver = BfsBruteForce::Solver.new
+moves  = solver.solve(KnivesState.new).moves
+
+puts moves
