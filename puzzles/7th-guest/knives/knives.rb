bf69609582dda6d5e0da6b93a4be799a5421af2c
Joe Sortelli <joe@sortelli.com>
2014-12-22 07:05:32 -0800

    Update knives documentation

--- puzzles/7th-guest/knives/knives.rb
+++ puzzles/7th-guest/knives/knives.rb
@@ -6,17 +6,21 @@ require 'bfs_brute_force'
 #
 # Remove all but one knife.
 #
-# Knives are initially arranged in a pentagram
+# Knives are initially arranged in a pentagram, with
+# only spot 0 being empty.
 #
-#         0 
+#         0
 #        / \
 #   1 - 3 - 4 - 2
 #    \ /     \ /
 #     5       6
-#    /  \   /  \ 
-#   /   / 7 \   \
-#  /  _/     \_  \
-#  8 /          \ 9
+#    /  \ 7 /  \
+#    8 /     \ 9
+#
+#
+# A knife can only move if it is immediately next to another knife,
+# and can hop over that knife and land on an empty spot. This move
+# will remove the knife that was hopped over (similar to checkers).
 #
 # This is the "Knife" puzzle from an old video game, The 7th Guest.
 
